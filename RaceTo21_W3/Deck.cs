744211ecf719937acad507d26e081b872ced1e2c
mahamismail <maham.ismail95@gmail.com>
2023-02-07 00:57:18 -0500

    Initial commit

---
+++ RaceTo21_W3/Deck.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // currently only needed if we use alternate shuffle method
+
+namespace RaceTo21
+{
+    public class Deck
+    {
+        List<Card> cards = new List<Card>();
+
+        public Deck()
+        {
+            Console.WriteLine("*********** Building deck...");
+            string[] suits = { "Spades", "Hearts", "Clubs", "Diamonds" };
+            int cardCounter = 0;
+
+            Dictionary<string, string> imageIDs = new Dictionary<string, string>();
+
+            for (int cardVal = 1; cardVal <= 13; cardVal++)
+            {
+                foreach (string cardSuit in suits)
+                {
+                    string cardName;
+                    string cardLongName;
+
+                    switch (cardVal)
+                    {
+                        case 1:
+                            cardName = "A";
+                            cardLongName = "Ace";
+                            break;
+                        case 11:
+                            cardName = "J";
+                            cardLongName = "Jack";
+                            break;
+                        case 12:
+                            cardName = "Q";
+                            cardLongName = "Queen";
+                            break;
+                        case 13:
+                            cardName = "K";
+                            cardLongName = "King";
+                            break;
+                        default:
+                            cardName = cardVal.ToString();
+                            cardLongName = cardName;
+                            break;
+                    }
+                    cards.Add(new Card(cardName + cardSuit.First<char>(), cardLongName + " of " + cardSuit));
+
+                    imageIDs[cards[cardCounter].id] = $"card_{cardSuit}_{cardName}.png";
+                    cardCounter++;
+
+                    Console.WriteLine($"card_{cardSuit}_{cardName}.png");
+
+
+
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Console.WriteLine("Shuffling Cards...");
+
+            Random rng = new Random();
+
+            // one-line method that uses Linq:
+            // cards = cards.OrderBy(a => rng.Next()).ToList();
+
+            // multi-line method that uses Array notation on a list!
+            // (this should be easier to understand)
+            for (int i=0; i<cards.Count; i++)
+            {
+                Card tmp = cards[i];
+                int swapindex = rng.Next(cards.Count);
+                cards[i] = cards[swapindex];
+                cards[swapindex] = tmp;
+
+                //cards[i] = cards[swapindex]; // makes a duplicate
+                //cards[swapindex] = cards[i]; //doesn't change anything
+            }
+        }
+
+        /* Maybe we can make a variation on this that's more useful,
+         * but at the moment it's just really to confirm that our 
+         * shuffling method(s) worked! And normally we want our card 
+         * table to do all of the displaying, don't we?!
+         */
+
+        public void ShowAllCards()
+        {
+            for (int i=0; i<cards.Count; i++)
+            {
+                Console.Write(i+":"+cards[i].displayName); // a list property can look like an Array!
+                if (i < cards.Count -1)
+                {
+                    Console.Write(" ");
+                } else
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        public Card DealTopCard()
+        {
+            Card card = cards[cards.Count - 1];
+            cards.RemoveAt(cards.Count - 1);
+            // Console.WriteLine("I'm giving you " + card);
+            return card;
+        }
+    }
+}
+
